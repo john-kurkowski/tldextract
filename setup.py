7d75c5aede5db19864e74bae4beea50378dd9060
John Kurkowski <john.kurkowski@gmail.com>
2011-02-27 17:24:41 -0800

    Make setup.py 2.5+ compatible

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import tldextract
 # I don't want to learn reStructuredText right now, so strip Markdown links
 # that make pip barf.
 long_description_markdown = tldextract.tldextract.__doc__
-long_description = re.sub(r'\[(.*?)\]\((http.*?)\)', r' \1 (\2)', long_description_markdown, flags=re.DOTALL)
+long_description = re.sub(r'(?s)\[(.*?)\]\((http.*?)\)', r'\1', long_description_markdown)
 
 setup(
     name = "tldextract",
@@ -23,6 +23,9 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
     ],
 )
 

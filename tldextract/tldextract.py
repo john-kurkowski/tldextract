1148410fa80af8bb021abbc99d159943a2c47e20
Marc Abramowitz <marc@marc-abramowitz.com>
2013-06-25 17:01:33 -0700

    Changes to support Python 3 using distribute's use_2to3=True

--- tldextract/tldextract.py
+++ tldextract/tldextract.py
@@ -1,25 +1,6 @@
 # -*- coding: utf-8 -*-
-"""`tldextract` accurately separates the gTLD or ccTLD (generic or country code
-top-level domain) from the registered domain and subdomains of a URL.
-
-    >>> import tldextract
-    >>> tldextract.extract('http://forums.news.cnn.com/')
-    ExtractResult(subdomain='forums.news', domain='cnn', tld='com')
-    >>> tldextract.extract('http://forums.bbc.co.uk/') # United Kingdom
-    ExtractResult(subdomain='forums', domain='bbc', tld='co.uk')
-    >>> tldextract.extract('http://www.worldbank.org.kg/') # Kyrgyzstan
-    ExtractResult(subdomain='www', domain='worldbank', tld='org.kg')
-
-`ExtractResult` is a namedtuple, so it's simple to access the parts you want.
-
-    >>> ext = tldextract.extract('http://forums.bbc.co.uk')
-    >>> ext.domain
-    'bbc'
-    >>> '.'.join(ext[:2]) # rejoin subdomain and domain
-    'forums.bbc'
-"""
-
 from __future__ import with_statement
+from .docstring import __doc__
 try:
     import cPickle as pickle
 except ImportError:

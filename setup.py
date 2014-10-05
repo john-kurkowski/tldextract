3b74898b1ca7e5b7259552d03bfe77a2fbaf399f
John Kurkowski <john.kurkowski@gmail.com>
2014-10-05 12:45:48 -0700

    Python 3.4 is cool too

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
     entry_points={
         'console_scripts': [

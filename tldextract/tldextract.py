556188b4ce2e53bdd1368125d5902c807c4e6217
Evan Volgas <evolgas@gmail.com>
2015-11-12 10:35:07 -0500

    strip it, strip it good

--- tldextract/tldextract.py
+++ tldextract/tldextract.py
@@ -187,6 +187,7 @@ class TLDExtract(object):
             .partition("#")[0] \
             .split("@")[-1] \
             .partition(":")[0] \
+            .strip() \
             .rstrip(".") \
             .strip()
 

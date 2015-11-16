40c1c3e139087c1d88551c06c1af96f825e0400b
John Kurkowski <john.kurkowski@gmail.com>
2015-11-15 18:00:35 -0800

    Embrace py.test style > JUnit

--- tldextract/tests/lint_test.py
+++ tldextract/tests/lint_test.py
@@ -1,11 +1,9 @@
 '''Lint tests.'''
 
-import itertools
 import os
 import pylint.epylint
 import re
 import sys
-import unittest
 
 from .helpers import check_output
 
@@ -13,74 +11,67 @@ if sys.version_info >= (3, 0):
     unicode = str # pylint: disable=invalid-name,redefined-builtin
 
 
-class PylintTestMeta(type):
-    '''Dynamically generates a test case for every Pylint error.'''
+SKIPPED_LINT = ()
+NO_LINT_ERRORS = ('',)
 
-    def __new__(mcs, name, bases, di):
-        '''Runs all Python files in this project through Pylint. For each lint
-        error, adds a test case method to `di`.'''
-        is_pylint_compatible = (2, 7) <= sys.version_info < (3, 5)
-        if not is_pylint_compatible:
-            return super(PylintTestMeta, mcs).__new__(mcs, name, bases, di)
 
-        project_root_cmd = ('git', 'rev-parse', '--show-toplevel')
-        project_root = unicode(check_output(project_root_cmd), 'utf-8').strip()
+def test_lint_error(lint_error):
+    assert not lint_error
 
-        ls_files_cmd = ('git', '--git-dir', os.path.join(project_root, '.git'), 'ls-files')
-        ls_files = (unicode(f, 'utf-8') for f in check_output(ls_files_cmd).splitlines())
-        py_files = (os.path.join(project_root, f) for f in ls_files if f.endswith('.py'))
 
-        pylint_errors = itertools.chain.from_iterable(
-            mcs.pylint_errors(project_root, py_file) for py_file in py_files
+def pytest_generate_tests(metafunc):
+    '''Runs all Python files in this project through Pylint. For each lint
+    error, adds corresponding failing test case.'''
+    is_pylint_compatible = (2, 7) <= sys.version_info < (3, 5)
+    if not is_pylint_compatible:
+        metafunc.parametrize('lint_error', SKIPPED_LINT)
+        return
+
+    project_root_cmd = ('git', 'rev-parse', '--show-toplevel')
+    project_root = unicode(check_output(project_root_cmd), 'utf-8').strip()
+
+    ls_files_cmd = ('git', '--git-dir', os.path.join(project_root, '.git'), 'ls-files')
+    ls_files = (unicode(f, 'utf-8') for f in check_output(ls_files_cmd).splitlines())
+    py_files = (os.path.join(project_root, f) for f in ls_files if f.endswith('.py'))
+
+    errors = [
+        error
+        for py_file in py_files
+        for error in pylint_errors(project_root, py_file)
+    ]
+
+    metafunc.parametrize('lint_error', errors or NO_LINT_ERRORS)
+
+
+def pylint_errors(project_root, py_file):
+    '''Yield Pylint errors from the given Python file.'''
+    # Match example:
+    # tldextract/tldextract.py:337: convention (C0111, missing-docstring, _PublicSuffixListTLDExtractor) Missing class docstring # pylint: disable=line-too-long
+    pylint_line_re = re.compile(r'''
+        \s*{}{}                     # Ignore absolute path outside project root
+        (?P<relative_line>
+        (?P<location>\S+\d+)        # File and lineno the error occurs
+        :\s*
+        (?P<category>\S+)           # e.g. convention, info, fatal
+        \s*
+        (?P<code>\(.+?\))           # Pylint code tuple
+        \s*
+        (?P<message>.+)             # Exact error message
         )
-        for pylint_error in pylint_errors:
-            test_name = 'test {} {}'.format(
-                pylint_error.group('location'),
-                pylint_error.group('category')
-            )
-            test_msg = pylint_error.group('message')
-            test_method = mcs.generate_lint_fail_method(test_msg)
-            di[test_name] = test_method
+    '''.format(project_root, os.path.sep), re.VERBOSE)
 
-        return type.__new__(mcs, name, bases, di)
+    exclude_categories = set(('info',))
 
-    @classmethod
-    def generate_lint_fail_method(mcs, test_msg):
-        def lint_fail(self):
-            self.assertTrue(False, test_msg)
-        return lint_fail
+    pylintrc = os.path.join(project_root, 'pylintrc')
+    pylint_opts = '{} --rcfile={}'.format(py_file, pylintrc)
+    stdout, stderr = pylint.epylint.py_run(pylint_opts, return_std=True)
 
-    @classmethod
-    def pylint_errors(mcs, project_root, py_file):
-        '''Yield Pylint errors as instances of re.MatchObject.'''
-        # Match example:
-        # tldextract/tldextract.py:337: convention (C0111, missing-docstring, _PublicSuffixListTLDExtractor) Missing class docstring # pylint: disable=line-too-long
-        pylint_line_re = re.compile(r'''
-            \s*{}{}                     # Ignore absolute path outside project root
-            (?P<location>\S+\d+)        # File and lineno the error occurs
-            :\s*
-            (?P<category>\S+)           # e.g. convention, info, fatal
-            \s*
-            (?P<code>\(.+?\))           # Pylint code tuple
-            \s*
-            (?P<message>.+)             # Exact error message
-        '''.format(project_root, os.path.sep), re.VERBOSE)
+    stderr_str = stderr.read()
+    looks_like_pylint_exit_error = len(stderr_str) > 200
+    if looks_like_pylint_exit_error:
+        raise Exception('`pylint {}` exited with error\n{}'.format(pylint_opts, stderr_str))
 
-        exclude_categories = set(('info',))
-
-        pylintrc = os.path.join(project_root, 'pylintrc')
-        pylint_opts = '{} --rcfile={}'.format(py_file, pylintrc)
-        stdout, stderr = pylint.epylint.py_run(pylint_opts, return_std=True)
-
-        stderr_str = stderr.read()
-        looks_like_pylint_exit_error = len(stderr_str) > 200
-        if looks_like_pylint_exit_error:
-            raise Exception('`pylint {0}` exited with error\n{1}'.format(pylint_opts, stderr_str))
-
-        for line in stdout.readlines():
-            match = pylint_line_re.match(line)
-            if match and match.group('category') not in exclude_categories:
-                yield match
-
-
-PylintTest = PylintTestMeta(str('PylintTest'), (unittest.TestCase,), {}) # pylint: disable=invalid-name
+    for line in stdout.readlines():
+        match = pylint_line_re.match(line)
+        if match and match.group('category') not in exclude_categories:
+            yield match.group('relative_line')
--- tldextract/tests/main_test.py
+++ tldextract/tests/main_test.py
@@ -4,9 +4,9 @@
 
 import logging
 import os
+import pytest
 import tempfile
 import traceback
-import unittest
 
 import tldextract
 
@@ -48,214 +48,241 @@ extract_using_extra_suffixes = tldextract.TLDExtract(
 # pylint: enable=invalid-name
 
 
-class TldextractTestCase(unittest.TestCase):
+@pytest.fixture(autouse=True)
+def reset_log_level():
+    logging.getLogger().setLevel(logging.WARN)
 
-    def setUp(self):
-        logging.getLogger().setLevel(logging.WARN)
+
+# Integration Tests
 
 
-class IntegrationTest(TldextractTestCase):
+def test_log_snapshot_diff():
+    logging.getLogger().setLevel(logging.DEBUG)
 
-    def test_log_snapshot_diff(self): # pylint: disable=no-self-use
-        logging.getLogger().setLevel(logging.DEBUG)
+    extractor = tldextract.TLDExtract()
+    try:
+        os.remove(extractor.cache_file)
+    except (IOError, OSError):
+        logging.warning(traceback.format_exc())
 
-        extractor = tldextract.TLDExtract()
-        try:
-            os.remove(extractor.cache_file)
-        except (IOError, OSError):
-            logging.warning(traceback.format_exc())
+    # TODO: if .tld_set_snapshot is up to date, this won't trigger a diff
+    extractor('ignore.com')
 
-        # TODO: if .tld_set_snapshot is up to date, this won't trigger a diff
-        extractor('ignore.com')
 
-    def test_bad_kwargs(self):
-        self.assertRaises(
-            ValueError,
-            tldextract.TLDExtract,
+def test_bad_kwargs():
+    with pytest.raises(ValueError):
+        tldextract.TLDExtract(
             cache_file=False, suffix_list_url=False, fallback_to_snapshot=False
         )
 
-    def test_fetch_and_suffix_list_conflict(self):
-        """ Make sure we support both fetch and suffix_list_url kwargs for this version.
 
-        GitHub issue #41.
-        """
-        extractor = tldextract.TLDExtract(suffix_list_url='foo', fetch=False)
-        self.assertFalse(extractor.suffix_list_urls)
+def test_fetch_and_suffix_list_conflict():
+    """ Make sure we support both fetch and suffix_list_url kwargs for this version.
 
-
-class ExtractTest(TldextractTestCase): # pylint: disable=too-many-public-methods
-
-    def assertExtract( # pylint: disable=invalid-name,too-many-arguments
-            self,
-            expected_subdomain,
-            expected_domain,
-            expected_tld,
-            url,
-            funs=(
-                extract,
-                extract_no_cache,
-                extract_using_real_local_suffix_list,
-                extract_using_real_local_suffix_list_no_cache,
-                extract_using_fallback_to_snapshot_no_cache
-            )):
-
-        for fun in funs:
-            ext = fun(url)
-            self.assertEquals(expected_subdomain, ext.subdomain)
-            self.assertEquals(expected_domain, ext.domain)
-            self.assertEquals(expected_tld, ext.tld)
-
-    def test_american(self):
-        self.assertExtract('www', 'google', 'com', 'http://www.google.com')
-
-    def test_british(self):
-        self.assertExtract("www", "theregister", "co.uk", "http://www.theregister.co.uk")
-
-    def test_no_subdomain(self):
-        self.assertExtract("", "gmail", "com", "http://gmail.com")
-
-    def test_nested_subdomain(self):
-        self.assertExtract("media.forums", "theregister", "co.uk",
-                           "http://media.forums.theregister.co.uk")
-
-    def test_odd_but_possible(self):
-        self.assertExtract('www', 'www', 'com', 'http://www.www.com')
-        self.assertExtract('', 'www', 'com', 'http://www.com')
-
-    def test_local_host(self):
-        self.assertExtract(
-            '', 'internalunlikelyhostname', '',
-            'http://internalunlikelyhostname/'
-        )
-        self.assertExtract(
-            'internalunlikelyhostname', 'bizarre', '',
-            'http://internalunlikelyhostname.bizarre'
-        )
-
-    def test_qualified_local_host(self):
-        self.assertExtract(
-            '', 'internalunlikelyhostname', 'info',
-            'http://internalunlikelyhostname.info/'
-        )
-        self.assertExtract(
-            'internalunlikelyhostname', 'information', '',
-            'http://internalunlikelyhostname.information/'
-        )
-
-    def test_ip(self):
-        self.assertExtract('', '216.22.0.192', '', 'http://216.22.0.192/')
-        self.assertExtract('216.22', 'project', 'coop', 'http://216.22.project.coop/')
-
-    def test_looks_like_ip(self):
-        self.assertExtract('', u'1\xe9', '', u'1\xe9')
-
-    def test_punycode(self):
-        self.assertExtract(
-            '', 'xn--h1alffa9f', 'xn--p1ai',
-            'http://xn--h1alffa9f.xn--p1ai'
-        )
-        # Entries that might generate UnicodeError exception
-        # This subdomain generates UnicodeError 'IDNA does not round-trip'
-        self.assertExtract(
-            'xn--tub-1m9d15sfkkhsifsbqygyujjrw602gk4li5qqk98aca0w', 'google', 'com',
-            'xn--tub-1m9d15sfkkhsifsbqygyujjrw602gk4li5qqk98aca0w.google.com'
-        )
-        # This subdomain generates UnicodeError 'incomplete punicode string'
-        self.assertExtract(
-            'xn--tub-1m9d15sfkkhsifsbqygyujjrw60', 'google', 'com',
-            'xn--tub-1m9d15sfkkhsifsbqygyujjrw60.google.com'
-        )
-
-    def test_empty(self):
-        self.assertExtract('', '', '', 'http://')
-
-    def test_scheme(self):
-        self.assertExtract('mail', 'google', 'com', 'https://mail.google.com/mail')
-        self.assertExtract('mail', 'google', 'com', 'ssh://mail.google.com/mail')
-        self.assertExtract('mail', 'google', 'com', '//mail.google.com/mail')
-        self.assertExtract('mail', 'google', 'com', 'mail.google.com/mail', funs=(extract,))
-
-    def test_port(self):
-        self.assertExtract('www', 'github', 'com', 'git+ssh://www.github.com:8443/')
-
-    def test_username(self):
-        self.assertExtract('1337', 'warez', 'com', 'ftp://johndoe:5cr1p7k1dd13@1337.warez.com:2501')
-
-    def test_query_fragment(self):
-        self.assertExtract('', 'google', 'com', 'http://google.com?q=cats')
-        self.assertExtract('', 'google', 'com', 'http://google.com#Welcome')
-        self.assertExtract('', 'google', 'com', 'http://google.com/#Welcome')
-        self.assertExtract('', 'google', 'com', 'http://google.com/s#Welcome')
-        self.assertExtract('', 'google', 'com', 'http://google.com/s?q=cats#Welcome')
-
-    def test_regex_order(self):
-        self.assertExtract('www', 'parliament', 'uk', 'http://www.parliament.uk')
-        self.assertExtract('www', 'parliament', 'co.uk', 'http://www.parliament.co.uk')
-
-    def test_unhandled_by_iana(self):
-        self.assertExtract('www', 'cgs', 'act.edu.au', 'http://www.cgs.act.edu.au/')
-        self.assertExtract('www', 'google', 'com.au', 'http://www.google.com.au/')
-
-    def test_tld_is_a_website_too(self):
-        self.assertExtract('www', 'metp', 'net.cn', 'http://www.metp.net.cn')
-        # self.assertExtract('www', 'net', 'cn', 'http://www.net.cn') # This is unhandled by the
-        # PSL. Or is it?
-
-    def test_dns_root_label(self):
-        self.assertExtract('www', 'example', 'com', 'http://www.example.com./')
-
-    def test_private_domains(self):
-        self.assertExtract('waiterrant', 'blogspot', 'com', 'http://waiterrant.blogspot.com')
-
-    def test_invalid_puny_with_puny(self):
-        self.assertExtract(
-            'xn--zckzap6140b352by.blog', 'so-net', 'xn--wcvs22d.hk',
-            'http://xn--zckzap6140b352by.blog.so-net.xn--wcvs22d.hk'
-        )
-
-    def test_puny_with_non_puny(self):
-        self.assertExtract(
-            'xn--zckzap6140b352by.blog', 'so-net', u'教育.hk',
-            u'http://xn--zckzap6140b352by.blog.so-net.教育.hk'
-        )
+    GitHub issue #41.
+    """
+    extractor = tldextract.TLDExtract(suffix_list_url='foo', fetch=False)
+    assert not extractor.suffix_list_urls
 
 
-class ExtractTestUsingCustomSuffixListFile(TldextractTestCase):
-
-    def test_suffix_which_is_not_in_custom_list(self):
-        for fun in (extract_using_fake_suffix_list, extract_using_fake_suffix_list_no_cache):
-            result = fun("www.google.com")
-            self.assertEquals(result.suffix, "")
-
-    def test_custom_suffixes(self):
-        for fun in (extract_using_fake_suffix_list, extract_using_fake_suffix_list_no_cache):
-            for custom_suffix in ('foo', 'bar', 'baz'):
-                result = fun("www.foo.bar.baz.quux" + "." + custom_suffix)
-                self.assertEquals(result.suffix, custom_suffix)
+# Extraction Unit Tests
 
 
-class ExtractTestUsingExtraSuffixes(TldextractTestCase):
+def assert_extract(
+        expected_subdomain,
+        expected_domain,
+        expected_tld,
+        url,
+        funs=(
+            extract,
+            extract_no_cache,
+            extract_using_real_local_suffix_list,
+            extract_using_real_local_suffix_list_no_cache,
+            extract_using_fallback_to_snapshot_no_cache
+        )):
 
-    def test_suffix_which_is_not_in_extra_list(self):
-        result = extract_using_extra_suffixes("www.google.com")
-        self.assertEquals(result.suffix, "")
-
-    def test_extra_suffixes(self):
-        for custom_suffix in EXTRA_SUFFIXES:
-            netloc = "www.foo.bar.baz.quux" + "." + custom_suffix
-            result = extract_using_extra_suffixes(netloc)
-            self.assertEquals(result.suffix, custom_suffix)
+    for fun in funs:
+        ext = fun(url)
+        assert expected_subdomain == ext.subdomain
+        assert expected_domain == ext.domain
+        assert expected_tld == ext.tld
 
 
-class ExtractTestAsDict(TldextractTestCase):
+def test_american():
+    assert_extract('www', 'google', 'com', 'http://www.google.com')
 
-    def test_result_as_dict(self):
-        result = extract(
-            "http://admin:password1@www.google.com:666"
-            "/secret/admin/interface?param1=42"
-        )
-        expected_dict = {'subdomain' : 'www',
-                         'domain' : 'google',
-                         'suffix' : 'com'}
-        self.assertEquals(result._asdict(), expected_dict)
+
+def test_british():
+    assert_extract("www", "theregister", "co.uk", "http://www.theregister.co.uk")
+
+
+def test_no_subdomain():
+    assert_extract("", "gmail", "com", "http://gmail.com")
+
+
+def test_nested_subdomain():
+    assert_extract("media.forums", "theregister", "co.uk",
+                   "http://media.forums.theregister.co.uk")
+
+
+def test_odd_but_possible():
+    assert_extract('www', 'www', 'com', 'http://www.www.com')
+    assert_extract('', 'www', 'com', 'http://www.com')
+
+
+def test_local_host():
+    assert_extract(
+        '', 'internalunlikelyhostname', '',
+        'http://internalunlikelyhostname/'
+    )
+    assert_extract(
+        'internalunlikelyhostname', 'bizarre', '',
+        'http://internalunlikelyhostname.bizarre'
+    )
+
+
+def test_qualified_local_host():
+    assert_extract(
+        '', 'internalunlikelyhostname', 'info',
+        'http://internalunlikelyhostname.info/'
+    )
+    assert_extract(
+        'internalunlikelyhostname', 'information', '',
+        'http://internalunlikelyhostname.information/'
+    )
+
+
+def test_ip():
+    assert_extract('', '216.22.0.192', '', 'http://216.22.0.192/')
+    assert_extract('216.22', 'project', 'coop', 'http://216.22.project.coop/')
+
+
+def test_looks_like_ip():
+    assert_extract('', u'1\xe9', '', u'1\xe9')
+
+
+def test_punycode():
+    assert_extract(
+        '', 'xn--h1alffa9f', 'xn--p1ai',
+        'http://xn--h1alffa9f.xn--p1ai'
+    )
+    # Entries that might generate UnicodeError exception
+    # This subdomain generates UnicodeError 'IDNA does not round-trip'
+    assert_extract(
+        'xn--tub-1m9d15sfkkhsifsbqygyujjrw602gk4li5qqk98aca0w', 'google', 'com',
+        'xn--tub-1m9d15sfkkhsifsbqygyujjrw602gk4li5qqk98aca0w.google.com'
+    )
+    # This subdomain generates UnicodeError 'incomplete punicode string'
+    assert_extract(
+        'xn--tub-1m9d15sfkkhsifsbqygyujjrw60', 'google', 'com',
+        'xn--tub-1m9d15sfkkhsifsbqygyujjrw60.google.com'
+    )
+
+
+def test_empty():
+    assert_extract('', '', '', 'http://')
+
+
+def test_scheme():
+    assert_extract('mail', 'google', 'com', 'https://mail.google.com/mail')
+    assert_extract('mail', 'google', 'com', 'ssh://mail.google.com/mail')
+    assert_extract('mail', 'google', 'com', '//mail.google.com/mail')
+    assert_extract('mail', 'google', 'com', 'mail.google.com/mail', funs=(extract,))
+
+
+def test_port():
+    assert_extract('www', 'github', 'com', 'git+ssh://www.github.com:8443/')
+
+
+def test_username():
+    assert_extract('1337', 'warez', 'com', 'ftp://johndoe:5cr1p7k1dd13@1337.warez.com:2501')
+
+
+def test_query_fragment():
+    assert_extract('', 'google', 'com', 'http://google.com?q=cats')
+    assert_extract('', 'google', 'com', 'http://google.com#Welcome')
+    assert_extract('', 'google', 'com', 'http://google.com/#Welcome')
+    assert_extract('', 'google', 'com', 'http://google.com/s#Welcome')
+    assert_extract('', 'google', 'com', 'http://google.com/s?q=cats#Welcome')
+
+
+def test_regex_order():
+    assert_extract('www', 'parliament', 'uk', 'http://www.parliament.uk')
+    assert_extract('www', 'parliament', 'co.uk', 'http://www.parliament.co.uk')
+
+
+def test_unhandled_by_iana():
+    assert_extract('www', 'cgs', 'act.edu.au', 'http://www.cgs.act.edu.au/')
+    assert_extract('www', 'google', 'com.au', 'http://www.google.com.au/')
+
+
+def test_tld_is_a_website_too():
+    assert_extract('www', 'metp', 'net.cn', 'http://www.metp.net.cn')
+    # assert_extract('www', 'net', 'cn', 'http://www.net.cn') # This is unhandled by the
+    # PSL. Or is it?
+
+def test_dns_root_label():
+    assert_extract('www', 'example', 'com', 'http://www.example.com./')
+
+
+def test_private_domains():
+    assert_extract('waiterrant', 'blogspot', 'com', 'http://waiterrant.blogspot.com')
+
+
+def test_invalid_puny_with_puny():
+    assert_extract(
+        'xn--zckzap6140b352by.blog', 'so-net', 'xn--wcvs22d.hk',
+        'http://xn--zckzap6140b352by.blog.so-net.xn--wcvs22d.hk'
+    )
+
+
+def test_puny_with_non_puny():
+    assert_extract(
+        'xn--zckzap6140b352by.blog', 'so-net', u'教育.hk',
+        u'http://xn--zckzap6140b352by.blog.so-net.教育.hk'
+    )
+
+
+# Custom Suffix List Tests
+
+
+def test_suffix_which_is_not_in_custom_list():
+    for fun in (extract_using_fake_suffix_list, extract_using_fake_suffix_list_no_cache):
+        result = fun("www.google.com")
+        assert result.suffix == ""
+
+
+def test_custom_suffixes():
+    for fun in (extract_using_fake_suffix_list, extract_using_fake_suffix_list_no_cache):
+        for custom_suffix in ('foo', 'bar', 'baz'):
+            result = fun("www.foo.bar.baz.quux" + "." + custom_suffix)
+            assert result.suffix == custom_suffix
+
+
+# Extra Suffixes Tests
+
+
+def test_suffix_which_is_not_in_extra_list():
+    result = extract_using_extra_suffixes("www.google.com")
+    assert result.suffix == ""
+
+
+def test_extra_suffixes():
+    for custom_suffix in EXTRA_SUFFIXES:
+        netloc = "www.foo.bar.baz.quux" + "." + custom_suffix
+        result = extract_using_extra_suffixes(netloc)
+        assert result.suffix == custom_suffix
+
+
+# _asdict Tests
+
+
+def test_result_as_dict():
+    result = extract(
+        "http://admin:password1@www.google.com:666"
+        "/secret/admin/interface?param1=42"
+    )
+    expected_dict = {'subdomain' : 'www',
+                     'domain' : 'google',
+                     'suffix' : 'com'}
+    assert result._asdict() == expected_dict

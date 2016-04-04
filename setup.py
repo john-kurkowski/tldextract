bd5d90f8bcc2e592afc29e449f04d121dfff34d2
John Kurkowski <john.kurkowski@gmail.com>
2016-04-03 19:28:23 -0700

    2.0rc1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if (2, 7) > sys.version_info:
 
 setup(
     name="tldextract",
-    version="1.7.5",
+    version="2.0rc1",
     author="John Kurkowski",
     author_email="john.kurkowski@gmail.com",
     description=("Accurately separate the TLD from the registered domain and"

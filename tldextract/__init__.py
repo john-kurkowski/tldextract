4697be8981d1bb22869ea88199e92a4bfe4615a1
Caleb Madrigal <caleb.madrigal@fireeye.com>
2017-08-17 10:30:58 -0500

    Added fake pkg_resources fallback in __init__

--- tldextract/__init__.py
+++ tldextract/__init__.py
@@ -1,6 +1,18 @@
 """Export tldextract's public interface."""
 
-import pkg_resources
+try:
+    import pkg_resources
+except ImportError:
+    class pkg_resources(object):  # pylint: disable=invalid-name
+
+        """Fake pkg_resources interface which falls back to getting resources
+        inside `tldextract`'s directory.
+        """
+        @classmethod
+        def resource_stream(cls, _, resource_name):
+            moddir = os.path.dirname(__file__)
+            path = os.path.join(moddir, resource_name)
+            return open(path)
 
 from .tldextract import extract, TLDExtract
 

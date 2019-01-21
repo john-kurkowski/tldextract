74ff35e5f9d81280830f753c45503a6de0292b6f
Hugo <hugovk@users.noreply.github.com>
2019-01-21 17:41:41 +0200

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
     packages=['tldextract'],
     include_package_data=True,
     long_description=LONG_DESCRIPTION,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",

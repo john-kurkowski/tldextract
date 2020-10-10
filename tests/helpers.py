eed31bcc2c9f0abb232b92a9198d667bd8c2ed4f
John Kurkowski <john.kurkowski@gmail.com>
2020-10-09 19:17:07 -0700

    Remove unused

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,21 +4,6 @@ from subprocess import CalledProcessError, PIPE, Popen
 import tempfile
 
 
-def check_output(*popenargs, **kwargs):
-    '''Copied from Python 2.7.'''
-    if 'stdout' in kwargs:
-        raise ValueError('stdout argument not allowed, it will be overridden.')
-    process = Popen(stdout=PIPE, *popenargs, **kwargs)
-    output, _ = process.communicate()
-    retcode = process.poll()
-    if retcode:
-        cmd = kwargs.get("args")
-        if cmd is None:
-            cmd = popenargs[0]
-        raise CalledProcessError(retcode, cmd, output=output)
-    return output
-
-
 def temporary_dir():
     """ Make a writable temporary file and return its absolute path.
     """

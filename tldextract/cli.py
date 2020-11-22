9f48e928ee906c9823fe8340e3065a597b9d7646
John Kurkowski <john.kurkowski@gmail.com>
2020-11-22 12:15:58 -0800

    Format

--- tldextract/cli.py
+++ tldextract/cli.py
@@ -1,32 +1,46 @@
-'''tldextract CLI'''
+"""tldextract CLI"""
 
 
 import argparse
 import logging
 import sys
 
-from .tldextract import TLDExtract
 from ._version import version as __version__
+from .tldextract import TLDExtract
 
 
 def main():
-    '''tldextract CLI main command.'''
+    """tldextract CLI main command."""
     logging.basicConfig()
 
     parser = argparse.ArgumentParser(
-        prog='tldextract',
-        description='Parse hostname from a url or fqdn')
+        prog="tldextract", description="Parse hostname from a url or fqdn"
+    )
 
-    parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
-    parser.add_argument('input', metavar='fqdn|url',
-                        type=str, nargs='*', help='fqdn or url')
+    parser.add_argument(
+        "--version", action="version", version="%(prog)s " + __version__
+    )
+    parser.add_argument(
+        "input", metavar="fqdn|url", type=str, nargs="*", help="fqdn or url"
+    )
 
-    parser.add_argument('-u', '--update', default=False, action='store_true',
-                        help='force fetch the latest TLD definitions')
-    parser.add_argument('-c', '--cache_dir',
-                        help='use an alternate TLD definition caching folder')
-    parser.add_argument('-p', '--private_domains', default=False, action='store_true',
-                        help='Include private domains')
+    parser.add_argument(
+        "-u",
+        "--update",
+        default=False,
+        action="store_true",
+        help="force fetch the latest TLD definitions",
+    )
+    parser.add_argument(
+        "-c", "--cache_dir", help="use an alternate TLD definition caching folder"
+    )
+    parser.add_argument(
+        "-p",
+        "--private_domains",
+        default=False,
+        action="store_true",
+        help="Include private domains",
+    )
 
     args = parser.parse_args()
     tld_extract = TLDExtract(include_psl_private_domains=args.private_domains)
@@ -42,4 +56,4 @@ def main():
         return
 
     for i in args.input:
-        print(' '.join(tld_extract(i)))
+        print(" ".join(tld_extract(i)))

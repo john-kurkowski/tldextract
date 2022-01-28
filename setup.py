f8b4eb82269853e1072b57894aae5568ad7263f1
whiteowl3 <71030468+whiteowl3@users.noreply.github.com>
2022-01-28 05:03:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",

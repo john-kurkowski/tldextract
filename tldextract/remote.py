284dcf022451ba56ad2b04dcf2e546c4577f4b10
Wu Tingfeng <wutingfeng@outlook.com>
2023-05-12 14:14:17 +0800

    Remove disable-next=line-too-long

--- tldextract/remote.py
+++ tldextract/remote.py
@@ -5,8 +5,8 @@ import socket
 from urllib.parse import scheme_chars
 
 IP_RE = re.compile(
-    # pylint: disable-next=line-too-long
-    r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+    r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.)"
+    r"{3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 )
 
 SCHEME_RE = re.compile(r"^([" + scheme_chars + "]+:)?//")

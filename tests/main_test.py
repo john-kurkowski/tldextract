5e6b5285ec8fa4ab448e00002d64551370e4ee6e
Wu Tingfeng <wutingfeng@outlook.com>
2023-05-16 17:25:54 +0800

    Add tests for _schemeless_url

--- tests/main_test.py
+++ tests/main_test.py
@@ -218,6 +218,8 @@ def test_scheme():
     assert_extract("://", ("", "", "", ""))
     assert_extract("://example.com", ("", "", "", ""))
     assert_extract("a+-.://example.com", ("example.com", "", "example", "com"))
+    assert_extract("a#//example.com", ("", "", "a", ""))
+    assert_extract("a@://example.com", ("", "", "", ""))
     assert_extract("#//example.com", ("", "", "", ""))
     assert_extract(
         "https://mail.google.com/mail", ("mail.google.com", "mail", "google", "com")

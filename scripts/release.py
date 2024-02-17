e5972cb88918a7765b732b8973f4b69caef6c502
Emily Corso <ekcorso@gmail.com>
2024-02-16 21:48:25 -0800

    Create release notes

--- scripts/release.py
+++ scripts/release.py
@@ -1,7 +1,9 @@
 import subprocess
 import sys
-# from github import Github
+import json
+import re
 from getpass import getpass
+
 
 def add_git_tag_for_version(version: str) -> None:
     """Add a git tag for the given version."""
@@ -62,9 +64,69 @@ def verify_build() -> None:
         sys.exit(1)
 
 
-# def get_github_client() -> Github:
-    """Get a Github client."""
-    
+def generate_github_release_notes_body(version) -> str:
+    """Generate and grab release notes URL from Github."""
+    try:
+        command = [
+            "curl",
+            "-L",
+            "-X",
+            "POST",
+            "-H",
+            "Accept: application/vnd.github+json",
+            "-H",
+            f"Authorization: Bearer {GITHUB_TOKEN}",
+            "-H",
+            "X-GitHub-Api-Version: 2022-11-28",
+            "https://api.github.com/repos/ekcorso/releasetestrepo2/releases/generate-notes",
+            "-d",
+            '{"tag_name":"${version_number}"}',
+        ]
+        response_json = subprocess.run(command, check=True, capture_output=True)
+        parsed_json = json.loads(response_json)
+        body = parsed_json["body"]
+        return body
+    except subprocess.CalledProcessError as error:
+        print(f"Failed to generate release notes: {error}")
+        return ""
+
+
+def get_release_notes_url(body) -> str:
+    """Parse the release notes content to get the changelog URL."""
+    url_pattern = re.compile(r"\*\*Full Changelog\*\*: (.*?)\n")
+    match = url_pattern.search(body)
+    if match:
+        return match.group(1)
+    else:
+        print("Failed to parse release notes URL from GitHub response.")
+        return ""
+
+
+# TODO: Refactor to use markdown parsing library instead of regex
+def get_changelog_release_notes() -> str:
+    """Get the changelog release notes."""
+    changelog_text = None
+    with open("../CHANGELOG.md", "r", encoding="utf-8") as file:
+        changelog_text = file.read()
+    pattern = re.compile(rf"## {version_number}(?:\n(.*?))?##", re.DOTALL)
+    match = pattern.search(changelog_text)
+    if match:
+        return match.group(1)
+    else:
+        print("Failed to parse changelog release notes.")
+        return ""
+
+
+def create_release_notes_body() -> str:
+    """Compile the release notes."""
+    changelog_notes = get_changelog_release_notes()
+    github_release_body = generate_github_release_notes_body(version_number)
+    release_notes_url = get_release_notes_url(github_release_body)
+    full_release_notes = (
+        changelog_notes + "\n\n**Full Changelog**: " + release_notes_url
+    )
+    return full_release_notes
+
 
 
 def upload_build_to_pypi() -> None:
@@ -86,21 +148,19 @@ def push_git_tags() -> None:
         print(f"Failed to push tags: {error}")
         sys.exit(1)
 
-"""
-pyPyToken = input("Enter the PyPI token: ")
-pyPyPassword = getpass("Enter the PyPI password: ")
-githubToken = getpass("Enter the github token: ")
-githubPassword = getpass("Enter the github password: ")
-"""
+
 version_number = input("Enter the version number: ")
+
 
 def main() -> None:
     """Run the main program."""
     print("Starting the upload process...")
+
     add_git_tag_for_version(version_number)
     remove_previous_dist()
     create_build()
     verify_build()
+
 
 if __name__ == "__main__":
     main()

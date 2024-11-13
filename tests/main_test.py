3d1bf184d4f20fbdbadd6274560ccd438939160e
John Kurkowski <john.kurkowski@gmail.com>
2024-11-13 13:25:36 -0800

    Consolidate 1-off test
    The "integration" test module has been shrinking over the years, and doesn't provide much distinction from the majority of the test suite.

--- tests/main_test.py
+++ tests/main_test.py
@@ -415,6 +415,14 @@ def test_ipv4_lookalike() -> None:
     )
 
 
+def test_bad_kwargs_no_way_to_fetch() -> None:
+    """Test an impossible combination of kwargs that disable all ways to fetch data."""
+    with pytest.raises(ValueError, match="disable all ways"):
+        tldextract.TLDExtract(
+            cache_dir=None, suffix_list_urls=(), fallback_to_snapshot=False
+        )
+
+
 def test_cache_permission(
     mocker: pytest_mock.MockerFixture, monkeypatch: pytest.MonkeyPatch, tmp_path: Path
 ) -> None:
